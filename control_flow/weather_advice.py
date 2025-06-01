da77690d5aeb04aa2f58d68b4725675b58d2f3ec
herodvillan <yusuf.d.kemal@gmail.com>
2025-06-01 09:56:38 +0300

    Delete control_flow directory

--- control_flow/weather_advice.py
+++
@@ -1,9 +0,0 @@
-x = input("What's the weather like today?(sunny/rainy/cold): ")
-if x ==  "sunny":
-    print("wear a t-shirt and sunglasses. ")
-elif x ==  "rainy":
-    print("Don't forget your umbrella and a raincoat. ")
-elif x ==  "cold":
-    print("Make sure to wear a warm coat and a scarf. ")
-else:
-    print("Sorry, I don't have recommendations for this weather. ")
