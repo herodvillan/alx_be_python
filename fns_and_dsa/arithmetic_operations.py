0b5b0768f57893b4fe13a21902d663f5ec956c45
Yusuf <yusuf.d.kemal@gmail.com>
2025-06-04 13:30:35 +0300

    i forgat the T

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,9 +1,7 @@
-
-
 def perform_operation(num1, num2, operation):
 	if operation == "add":
 		return num1 + num2
-	elif operation == "subsract":
+	elif operation == "subtract":
 		return num1 - num2
 	elif operation == "multiply":
 		return num1 * num2
