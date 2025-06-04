b34df96f48ff0a4bdc695af0e597b5612ff61b98
Yusuf <yusuf.d.kemal@gmail.com>
2025-06-04 13:25:29 +0300

    functions

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+
+
+def perform_operation(num1, num2, operation):
+	if operation == "add":
+		return num1 + num2
+	elif operation == "subsract":
+		return num1 - num2
+	elif operation == "multiply":
+		return num1 * num2
+	elif operation == "divide":
+		if num2 == 0:
+			return "undifined"
+		return num1 / num2
+	else:
+		return "Error"
+
