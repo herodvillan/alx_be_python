568b79d7ee9539175f56b178399ebaa522f76c90
Yusuf <yusuf.d.kemal@gmail.com>
2025-06-15 10:43:10 +0300

    gti

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,19 @@
+from simple_calculator import SimpleCalculator
+import unittest
+
+
+class TestCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = SimpleCalculator()
+    def test_addition(self):
+        self.assertEqual(self.calc.add(-2, 1), -1)
+
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(-2, 1), -3)
+
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(-2, -1), 2)
+
+    def test_division(self):
+        self.assertEqual(self.calc.divide(5, 2), 2.5)
